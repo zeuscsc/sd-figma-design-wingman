6cb5d6f42f8084c51e1c6d8809b2b74240a9856e
Zeus Chiu <zeuscsc@gmail.com>
2023-04-18 12:23:46 +0800

    control net for sketch picker and size limiter

--- code.ts
+++ code.ts
@@ -1,57 +1,62 @@
-let url="";
-const default_bad_prompt="text, logo, signature, over-saturated, over-exposed, amateur, extra limbs, extra barrel, b&w, close-up, duplicate, mutilated, extra fingers, mutated hands, deformed, blurry, bad proportions, extra limbs, cloned face, out of frame, bad anatomy, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, mutated hands, fused fingers, too many fingers, long neck, tripod, tube, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, nfixer"
+let url = "";
+let used_base_model="2.1/rmadaMergeSD21768_v60.safetensors [7da43996bb]";
+let control_net_for_sketch="control_sd21_scribble-sd21-safe [6e34c018]";
+const IDEAL_SIZE = 768;
+const default_bad_prompt = "nfixer,text, logo, signature, over-saturated, over-exposed, amateur, extra limbs, extra barrel, b&w, close-up, duplicate, mutilated, extra fingers, mutated hands, deformed, blurry, bad proportions, extra limbs, cloned face, out of frame, bad anatomy, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, mutated hands, fused fingers, too many fingers, long neck, tripod, tube, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, "
 figma.showUI(__html__, { width: 320, height: 720 });
 figma.ui.onmessage = async msg => {
+  if(msg.type==="check_availibility"){
+    url = msg.url;
+    used_base_model=await get_loaded_model();
+    await control_net_for_sketch_picker();
+  }
   if (msg.type === 'txt2img') {
     let width = msg.width;
     let height = msg.height;
     let style = msg.style;
-    url= msg.url;
+    url = msg.url;
     txt2img(msg.prompt, width, height, style);
   }
-  if (msg.type==="auto_mask"||msg.type==="prompt_mask"||msg.type==="vectors_2_image"){
+  if (msg.type === "auto_mask" || msg.type === "prompt_mask" || msg.type === "vectors_2_image") {
     let width = figma.currentPage.selection[0].width;
     let height = figma.currentPage.selection[0].height;
+    [width,height]=keep_aspect_ratio(width,height);
     let style = msg.style;
-    let imageUrl=msg.imageUrl;
-    let prompt=msg.prompt;
-    let vectors_guidance=msg.vectors_guidance;
-    url= msg.url;
-    if(msg.type==="auto_mask")auto_mask(imageUrl,"",width,height,style);
-    if(msg.type==="prompt_mask") prompt_mask(imageUrl,prompt,width,height,style);
-    if(msg.type==="vectors_2_image")vectors_2_image(imageUrl,prompt,width,height,style,vectors_guidance);
+    let imageUrl = msg.imageUrl;
+    let prompt = msg.prompt;
+    let vectors_guidance = msg.vectors_guidance;
+    url = msg.url;
+    if (msg.type === "auto_mask") auto_mask(imageUrl, "", width, height, style);
+    if (msg.type === "prompt_mask") prompt_mask(imageUrl, prompt, width, height, style);
+    if (msg.type === "vectors_2_image") vectors_2_image(imageUrl, prompt, width, height, style, vectors_guidance);
   }
-  if(msg.type==="render_sketch"){
-    let width = figma.currentPage.selection[0].width;
-    let height = figma.currentPage.selection[0].height;
-    let style = msg.style;
-
+  if(msg.type==="canny"){
+    get_canny(msg.imageUrl,msg.url,msg.canny_low_threshold,msg.canny_high_threshold);
   }
 
   if (msg.type === 'create_image_node') {
-    create_image_node(msg.original_task,msg.byte_array);
+    create_image_node(msg.original_task, msg.byte_array);
   }
   if (msg.type === 'get_selected_image') {
-    const original_task=msg.original_task;
-    const imageData=await get_selected_image();
-    figma.ui.postMessage({ type: 'image_selected',original_task, data: imageData });
+    const original_task = msg.original_task;
+    let imageData = await get_selected_image();
+    figma.ui.postMessage({ type: 'image_selected', original_task, data: imageData });
   }
   if (msg.type === 'create_node_from_svg') {
-    console.log(msg.svgstr)
     const svgString = msg.svgstr;
     const width = msg.width;
     const height = msg.height;
     const node = figma.createNodeFromSvg(svgString);
-    if(figma.currentPage.selection.length>0){
-      node.x=figma.currentPage.selection[0].x+100;
-      node.y=figma.currentPage.selection[0].y+100;
+    if (figma.currentPage.selection.length > 0) {
+      node.x = figma.currentPage.selection[0].x + 100;
+      node.y = figma.currentPage.selection[0].y + 100;
     }
     node.resize(width, height);
     figma.currentPage.appendChild(node);
     figma.currentPage.selection = [node];
   }
   if (msg.type === 'find_items') {
-    if(figma.currentPage.selection.length>0)figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
+    if (figma.currentPage.selection.length > 0) figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
   }
   if (msg.type === 'cancel')
     figma.closePlugin();
@@ -60,18 +65,17 @@ figma.on('selectionchange', () => {
   const selection = figma.currentPage.selection;
   figma.ui.postMessage({ type: 'sync_img2img_ui', selected_nodes_count: selection.length });
 });
-function initialize(){
+function initialize() {
   const selection = figma.currentPage.selection;
   figma.ui.postMessage({ type: 'on_initialized', selected_nodes_count: selection.length });
 }
 initialize()
 
-function api_results(original_task:string,base64: string) {
-  figma.ui.postMessage({ type: 'api_results',original_task:original_task, data: base64 });
+function api_results(original_task: string, base64: string) {
+  figma.ui.postMessage({ type: 'api_results', original_task: original_task, data: base64 });
 }
 function svg_results(svg: string, width: number = 512, height: number = 512) {
   const nodes: SceneNode[] = [];
-  console.log(svg)
   const svgNode = figma.createNodeFromSvg(svg);
   svgNode.resize(width, height);
   nodes.push(svgNode);
@@ -98,19 +102,19 @@ async function txt2img(prompt: string, width: number = 512, height: number = 512
     body: JSON.stringify(query)
   });
   let data = await res.json();
-  api_results("txt2img",data['images'][0])
+  api_results("txt2img", data['images'][0])
 }
-async function auto_mask(imageUrl:string,prompt:string,width: number = 512, height: number = 512, style: string = "default") {
+async function auto_mask(imageUrl: string, prompt: string, width: number = 512, height: number = 512, style: string = "default") {
   prompt = get_styled_prompt(prompt, style);
   let query: any = {
     "image_str": imageUrl,
-    "alpha_matting":false,
-    "alpha_matting_foreground_threshold":240,
-    "alpha_matting_background_threshold":10,
-    "alpha_matting_erode_size":10,
-    "session_name":"u2net",
-    "only_mask":false,
-    "post_process_mask":true,
+    "alpha_matting": false,
+    "alpha_matting_foreground_threshold": 240,
+    "alpha_matting_background_threshold": 10,
+    "alpha_matting_erode_size": 10,
+    "session_name": "u2net",
+    "only_mask": false,
+    "post_process_mask": true,
   }
   let res = await fetch(`${url}/auto_mask/remove-background`, {
     method: 'POST',
@@ -120,19 +124,19 @@ async function auto_mask(imageUrl:string,prompt:string,width: number = 512, heig
     body: JSON.stringify(query)
   });
   let data = await res.json();
-  api_results("auto_mask",data['mask'])
-  
+  api_results("auto_mask", data['mask'])
+
 }
-async function prompt_mask(imageUrl:string,prompt:string,width: number = 512, height: number = 512, style: string = "default"){
+async function prompt_mask(imageUrl: string, prompt: string, width: number = 512, height: number = 512, style: string = "default") {
   prompt = get_styled_prompt(prompt, style);
   let query: any = {
     "image_str": imageUrl,
-    "prompts":prompt,
-    "neg_prompts":"white background",
-    "only_mask":false,
-    "threshold":0.4,
-    "mask_blur_median":11,
-    "mask_blur_gaussian":11,
+    "prompts": prompt,
+    "neg_prompts": "white background",
+    "only_mask": false,
+    "threshold": 0.4,
+    "mask_blur_median": 11,
+    "mask_blur_gaussian": 11,
   }
   let res = await fetch(`${url}/prompt_mask/remove-background`, {
     method: 'POST',
@@ -142,13 +146,14 @@ async function prompt_mask(imageUrl:string,prompt:string,width: number = 512, he
     body: JSON.stringify(query)
   });
   let data = await res.json();
-  api_results("prompt_mask",data['mask'])
-  
+  api_results("prompt_mask", data['mask'])
+
 }
-async function vectors_2_image(imageUrl:string,prompt:string,width: number = 512, height: number = 512, style: string = "default",vectors_guidance:number=1) {
+async function vectors_2_image(imageUrl: string, prompt: string, width: number = 512, height: number = 512, style: string = "default", 
+                                  vectors_guidance: number = 1) {
   prompt = get_styled_prompt(prompt, style);
   let query: any = {
-    "prompt":prompt,
+    "prompt": prompt,
     "negative_prompt": default_bad_prompt,
     "steps": 30,
     "sampler_index": "Euler a",
@@ -159,11 +164,11 @@ async function vectors_2_image(imageUrl:string,prompt:string,width: number = 512
       "controlnet": {
         "args": [
           {
-            "input_image":`data:image/png;base64,${imageUrl}`,
+            "input_image": `data:image/png;base64,${imageUrl}`,
             "module": "scribble",
-            "model": "control_sd21_scribble-sd21-safe [6e34c018]",
-            "guessmode":false,
-            "weight":Number(vectors_guidance),
+            "model": control_net_for_sketch,
+            "guessmode": false,
+            "weight": Number(vectors_guidance),
           }
         ]
       }
@@ -177,10 +182,10 @@ async function vectors_2_image(imageUrl:string,prompt:string,width: number = 512
     body: JSON.stringify(query)
   });
   let data = await res.json();
-  api_results("vectors_2_image",data['images'][0])
-  
+  api_results("vectors_2_image", data['images'][0])
+
 }
-function get_styled_prompt(prompt:string,style:string){
+function get_styled_prompt(prompt: string, style: string) {
   switch (style) {
     case "photo":
       prompt = `.photo realistic, ultra details, natural light,
@@ -218,7 +223,24 @@ function get_styled_prompt(prompt:string,style:string){
   return prompt;
 }
 
-async function create_image_node(original_task:string,byte_array: Uint8Array) {
+async function get_canny(imageUrl: string,url:string,canny_low_threshold:number,canny_high_threshold:number) {
+  let query = {
+    image_str: imageUrl,
+    annotator_resolution: 512,
+    canny_low_threshold: canny_low_threshold,
+    canny_high_threshold: canny_high_threshold,
+  }
+  let res = await fetch(`${url}/figma/canny`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(query)
+  });
+  let data = await res.json();
+  api_results("get_canny", data['image'])
+}
+async function create_image_node(original_task: string, byte_array: Uint8Array) {
   // const nodes: SceneNode[] = [];
   const imageNode = figma.createRectangle();
   const image = await figma.createImage(byte_array);
@@ -231,114 +253,60 @@ async function create_image_node(original_task:string,byte_array: Uint8Array) {
       scaleMode: 'FILL'
     }
   ]
-  if(figma.currentPage.selection.length>0){
-    imageNode.x=figma.currentPage.selection[0].x+100;
-    imageNode.y=figma.currentPage.selection[0].y+100;
+  if (figma.currentPage.selection.length > 0) {
+    imageNode.x = figma.currentPage.selection[0].x + 100;
+    imageNode.y = figma.currentPage.selection[0].y + 100;
     // nodes.push(figma.currentPage.selection[0])
   }
   // nodes.push(imageNode);
   // figma.currentPage.selection = nodes;
   // figma.viewport.scrollAndZoomIntoView(nodes);
-  figma.ui.postMessage({ original_task,type: 'done' });
+  figma.ui.postMessage({ original_task, type: 'done' });
 }
 async function get_selected_image() {
   if (figma.currentPage.selection.length === 0) return
   return await figma.currentPage.selection[0].exportAsync({ format: "PNG", constraint: { type: "SCALE", value: 1 } })
 }
-function encodeURIComponent(input: string): string {
-  const hexChars = '0123456789ABCDEF';
-  let output = '';
+function keep_aspect_ratio(width:number,height:number){
+  let ratio=1;
+  if (width > height) {
+    ratio=IDEAL_SIZE/height;
+    height=IDEAL_SIZE;
+    width=width*ratio;
+    const newHeight = width * (height / width);
+    width = width;
+    height = newHeight;
+  } else {
+    ratio=IDEAL_SIZE/width;
+    width=IDEAL_SIZE;
+    height=height*ratio;
+    const newWidth = height * (width / height);
+    height = height;
+    width = newWidth;
+  }
+  return [width,height]
+}
 
-  for (let i = 0; i < input.length; i++) {
-    const charCode = input.charCodeAt(i);
 
-    if (
-      (charCode >= 65 && charCode <= 90) || // A-Z
-      (charCode >= 97 && charCode <= 122) || // a-z
-      (charCode >= 48 && charCode <= 57) || // 0-9
-      charCode === 45 || // -
-      charCode === 95 || // _
-      charCode === 46 || // .
-      charCode === 126 // ~
-    ) {
-      output += input[i];
-    } else {
-      const hexCode = charCode.toString(16).toUpperCase();
-      output += `%${hexCode.length === 1 ? '0' : ''}${hexCode}`;
+async function get_loaded_model() {
+  if(url==="")return
+  let res = await fetch(`${url}/sdapi/v1/options`);
+  let options = await res.json();
+  return options['sd_model_checkpoint'];
+}
+async function control_net_for_sketch_picker(){
+  if(url==="")return
+  let res = await fetch(`${url}/controlnet/model_list`);
+  let model_list = await res.json();
+  model_list=model_list['model_list'];
+  for(let model_name of model_list){
+    let controlNetAvailable=false
+    if(/1\.5\//.test(used_base_model)){ controlNetAvailable= /sd15/.test(model_name) && /scribble/.test(model_name);
+    }else if(/2\.1\//.test(used_base_model)){ controlNetAvailable= /sd21/.test(model_name) && /scribble/.test(model_name);
+    }
+    if (controlNetAvailable) {
+      control_net_for_sketch = model_name;
+      return
     }
   }
-
-  return output;
-}
-function btoa(input: string): string {
-  const base64Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-  let output = '';
-  let char1, char2, char3;
-  let enc1, enc2, enc3, enc4;
-  let i = 0;
-
-  input = encodeURIComponent(input);
-
-  do {
-    char1 = input.charCodeAt(i++);
-    char2 = input.charCodeAt(i++);
-    char3 = input.charCodeAt(i++);
-
-    enc1 = char1 >> 2;
-    enc2 = ((char1 & 3) << 4) | (char2 >> 4);
-    enc3 = ((char2 & 15) << 2) | (char3 >> 6);
-    enc4 = char3 & 63;
-
-    if (isNaN(char2)) {
-      enc3 = enc4 = 64;
-    } else if (isNaN(char3)) {
-      enc4 = 64;
-    }
-
-    output += base64Chars.charAt(enc1) + base64Chars.charAt(enc2) + base64Chars.charAt(enc3) + base64Chars.charAt(enc4);
-  } while (i < input.length);
-
-  return output;
-}
-function atob(input: string): string {
-  const base64Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-  let output = '';
-  let char1, char2, char3;
-  let enc1, enc2, enc3, enc4;
-  let i = 0;
-  input = input.replace(/\s/g, '');
-  const padding = input.length % 4;
-  if (padding > 0) {
-    input += '='.repeat(4 - padding);
-  }
-  do {
-    enc1 = base64Chars.indexOf(input.charAt(i++));
-    enc2 = base64Chars.indexOf(input.charAt(i++));
-    enc3 = base64Chars.indexOf(input.charAt(i++));
-    enc4 = base64Chars.indexOf(input.charAt(i++));
-
-    char1 = (enc1 << 2) | (enc2 >> 4);
-    char2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-    char3 = ((enc3 & 3) << 6) | enc4;
-
-    output += String.fromCharCode(char1);
-
-    if (enc3 !== 64) {
-      output += String.fromCharCode(char2);
-    }
-    if (enc4 !== 64) {
-      output += String.fromCharCode(char3);
-    }
-  } while (i < input.length);
-
-  return output;
-}
-function base64ToUint8Array(base64:string) {
-  var binary_string = atob(base64);
-  var len = binary_string.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-  return new Uint8Array(bytes.buffer);
 }
